118a09552821256eea6c7b108bd7665af1b822e6
localbody macmini <localbody@gmail.com>
2025-12-02 22:47:07 +0300

    begin html to components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
+import Aside from './components/layout/Aside.vue';
+import Main from './components/layout/Main.vue';
+
 
 </script>
 
 <template>
- 
+ <div class="container">
+    <Aside />
+    <Main />
+ </div>
 </template>
 
 <style scoped>
---
+++ src/components/layout/Aside.vue
@@ -0,0 +1,198 @@
+<script setup>
+import Header from '@/components/Header.vue';
+</script>
+
+<template>
+    <div class="aside">
+        <Header />
+        <!--  -->
+        <div class="quotes">
+            A well-organized, creative and goaloriented graduate possessing
+            excellent communication, problem-solving and leadership skills with a
+            flair to explore Computer Science Engineering
+        </div>
+        <!--  -->
+        <hr />
+        <!--  -->
+        <div class="rows rows--infos">
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/mail.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Email</span>
+                    <div class="row__subtitle">
+                        <a class="row__link" href="mailto:anuragyadavmnp@gmail.com">anuragyadavmnp@gmail.com</a>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/link.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Website</span>
+                    <div class="row__subtitle">
+                        <a class="row__link row__link--underline"
+                            href="https://anuragyadav365.github.io/portfolio.html">https://anuragyadav365.github.io/portfolio.html</a>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/phone.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Phone</span>
+                    <div class="row__subtitle">
+                        <a class="row__link" href="tel:+(+91) 981 5533 309">(+91) 981 5533 309</a>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/location.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Address</span>
+                    <div class="row__subtitle">
+                        <span class="row__text">Hallomajra, Chandigarh, India</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--  -->
+        <hr />
+        <!--  -->
+        <div class="rows rows--socials">
+            <span class="rows__title">Socials</span>
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/instagram.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Instagram</span>
+                    <div class="row__subtitle">
+                        <a class="row__link" href="@anurag.yadav.007">@anurag.yadav.007</a>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/github.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Github</span>
+                    <div class="row__subtitle">
+                        <a class="row__link" href="@anuragyadav365">@anuragyadav365</a>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/twitter.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Twitter</span>
+                    <div class="row__subtitle">
+                        <a class="row__link" href="@anuragmnp">@anuragmnp</a>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/linkdin.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Linkedin</span>
+                    <div class="row__subtitle">
+                        <a class="row__link" href="@anuragyadav">@anuragyadav</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--  -->
+        <hr />
+        <div class="rows rows--languages">
+            <span class="rows__title"> Languages </span>
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/hindi.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Hindi</span>
+                    <div class="row__subtitle">
+                        <span class="row__text">Native</span>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/english.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">English</span>
+                    <div class="row__subtitle">
+                        <span class="row__text">Professional working</span>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/spanish.svg" alt="" />
+                </div>
+                <div class="row__wrapper">
+                    <span class="row__title">Spanish</span>
+                    <div class="row__subtitle">
+                        <span class="row__text">Elementary</span>
+                    </div>
+                </div>
+            </div>
+            <!--  -->
+        </div>
+        <!--  -->
+        <hr />
+        <!--  -->
+        <div class="rows rows--hobbies-interests">
+            <span class="rows__title">Hobbies & Interests</span>
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/gaming.svg" alt="" />
+                </div>
+                <span class="row__subtitle">Gaming</span>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/travelling.svg" alt="" />
+                </div>
+                <span class="row__subtitle">Travelling</span>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/sketching.svg" alt="" />
+                </div>
+                <span class="row__subtitle">Sketching/Graffitti</span>
+            </div>
+            <!--  -->
+            <div class="row">
+                <div class="row__icon">
+                    <img src="/icons/blogging.svg" alt="" />
+                </div>
+                <span class="row__subtitle">Blogging</span>
+            </div>
+            <!--  -->
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
