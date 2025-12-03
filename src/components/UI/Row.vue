86b5e6ce0e1d5eead85c002cefbede5d84dd63d5
localbody macmini <localbody@gmail.com>
2025-12-03 22:07:57 +0300

    aside complite

--- src/components/UI/Row.vue
+++ src/components/UI/Row.vue
@@ -1,37 +1,40 @@
 <script>
 export default {
-    name: 'Row',
-    props: {
-        icon: {
-            type: String,
-            required: true,
-        },
-        title: {
-            type: String,
-            required: true,
-        },
-        link: {
-            type: Object,
-            required: true,
-        }
+  name: 'Row',
+  props: {
+    icon: {
+      type: String,
+      required: true,
     },
+    title: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: Object,
+      required: true,
+    },
+  },
 }
 </script>
 
 <template>
-    <div class="row">
-        <div class="row__icon">
-            <img :src="icon" alt="icon" />
-        </div>
-        <div class="row__wrapper">
-            <span class="row__title">{{ title }}</span>
-            <div class="row__subtitle">
-
-                <a v-if="link.href != null" class="row__link" :href="link.href">{{ link.title }}</a>
-                <span v-else class="row__text">{{ link.title }}</span>
-            </div>
-        </div>
+  <div class="row">
+    <div class="row__icon">
+      <img :src="icon" alt="icon" />
     </div>
+    <div class="row__wrapper">
+      <span v-if="link != null" class="row__title">{{ title }}</span>
+      <div class="row__subtitle">
+        <!-- *normal? -->
+        <template v-if="link == null">{{ title }}</template>
+        <a v-else-if="link.href != null" class="row__link" :href="link.href">{{
+          link.title
+        }}</a>
+        <span v-else class="row__text">{{ link.title }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- src/components/layout/Aside.vue
+++ src/components/layout/Aside.vue
@@ -1,154 +1,175 @@
 <script setup>
-import Header from '@/components/Header.vue';
-import Quotes from '../UI/Quotes.vue';
-import Row from '../UI/Row.vue';
+import Header from '@/components/Header.vue'
+import Quotes from '../UI/Quotes.vue'
+import Row from '../UI/Row.vue'
+
+const itemsRows = {
+  infos: {
+    items: [
+      {
+        icon: '/icons/mail.svg',
+        title: 'Email',
+        link: {
+          href: 'mailto:anuragyadavmnp@gmail.com',
+          title: 'anuragyadavmnp@gmail.com',
+        },
+      },
+      {
+        icon: '/icons/link.svg',
+        title: 'Website',
+        link: {
+          href: 'https://anuragyadav365.github.io/portfolio.html',
+          title: 'https://anuragyadav365.github.io/portfolio.html',
+        },
+      },
+      {
+        icon: '/icons/phone.svg',
+        title: 'Phone',
+        link: { href: 'tel:+(+91) 981 5533 309', title: '(+91) 981 5533 309' },
+      },
+      {
+        icon: '/icons/location.svg',
+        title: 'Address',
+        link: { title: 'Hallomajra, Chandigarh, India' },
+      },
+    ],
+  },
+  socials: {
+    items: [
+      {
+        icon: '/icons/instagram.svg',
+        title: 'Instagram',
+        link: {
+          href: '@anurag.yadav.007',
+          title: '@anurag.yadav.007',
+        },
+      },
+      {
+        icon: '/icons/github.svg',
+        title: 'Github',
+        link: {
+          href: '@anuragyadav365',
+          title: '@anuragyadav365',
+        },
+      },
+      {
+        icon: '/icons/twitter.svg',
+        title: 'Twitter',
+        link: {
+          href: '@anuragmnp',
+          title: '@anuragmnp',
+        },
+      },
+      {
+        icon: '/icons/linkdin.svg',
+        title: 'Linkedin',
+        link: {
+          href: '@anuragyadav',
+          title: '@anuragyadav',
+        },
+      },
+    ],
+  },
+  languages: {
+    items: [
+      {
+        icon: '/icons/hindi.svg',
+        title: 'Hindi',
+        link: {
+          title: 'Native',
+        },
+      },
+      {
+        icon: '/icons/english.svg',
+        title: 'English',
+        link: {
+          title: 'Professional working',
+        },
+      },
+      {
+        icon: '/icons/spanish.svg',
+        title: 'Spanish',
+        link: {
+          title: 'Elementary',
+        },
+      },
+    ],
+  },
+  hobbies_interests: {
+    items: [
+      {
+        icon: '/icons/gaming.svg',
+        title: 'Gaming',
+      },
+      {
+        icon: '/icons/travelling.svg',
+        title: 'Travelling',
+      },
+      {
+        icon: '/icons/sketching.svg',
+        title: 'Sketching',
+      },
+      {
+        icon: '/icons/blogging.svg',
+        title: 'Blogging',
+      },
+    ],
+  },
+}
 </script>
 
 <template>
-    <div class="aside">
-        <Header src="/images/photo.jpg" name="Anurag Yadav" alt="Anurag Yadav" />
-        <!--  -->
-        <Quotes text="A well-organized, creative and goaloriented graduate possessing
+  <div class="aside">
+    <Header src="/images/photo.jpg" name="Anurag Yadav" alt="Anurag Yadav" />
+    <Quotes
+      text="A well-organized, creative and goaloriented graduate possessing
         excellent communication, problem-solving and leadership skills with a
-        flair to explore Computer Science Engineering" /> 
-        <!--  -->
-        <hr />
-        <!--  -->
-        <div class="rows rows--infos">
-            <Row icon="/icons/mail.svg" title="Email" :link="{ href: 'mailto:anuragyadavmnp@gmail.com', title: 'anuragyadavmnp@gmail.com' }" />
-            <Row icon="/icons/link.svg" title="Website" :link="{ href:'https://anuragyadav365.github.io/portfolio.html', title: 'https://anuragyadav365.github.io/portfolio.html' }" />
-            <Row icon="/icons/phone.svg" title="Phone" :link="{ href:'tel:+(+91) 981 5533 309', title: '(+91) 981 5533 309' }" />
-            <Row icon="/icons/location.svg" title="Address" :link= "{ title: 'Hallomajra, Chandigarh, India' }" />
-        </div>
-        <!--  -->
-        <hr />
-        <!--  -->
-        <div class="rows rows--socials">
-            <span class="rows__title">Socials</span>
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/instagram.svg" alt="" />
-                </div>
-                <div class="row__wrapper">
-                    <span class="row__title">Instagram</span>
-                    <div class="row__subtitle">
-                        <a class="row__link" href="@anurag.yadav.007">@anurag.yadav.007</a>
-                    </div>
-                </div>
-            </div>
-            <!--  -->
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/github.svg" alt="" />
-                </div>
-                <div class="row__wrapper">
-                    <span class="row__title">Github</span>
-                    <div class="row__subtitle">
-                        <a class="row__link" href="@anuragyadav365">@anuragyadav365</a>
-                    </div>
-                </div>
-            </div>
-            <!--  -->
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/twitter.svg" alt="" />
-                </div>
-                <div class="row__wrapper">
-                    <span class="row__title">Twitter</span>
-                    <div class="row__subtitle">
-                        <a class="row__link" href="@anuragmnp">@anuragmnp</a>
-                    </div>
-                </div>
-            </div>
-            <!--  -->
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/linkdin.svg" alt="" />
-                </div>
-                <div class="row__wrapper">
-                    <span class="row__title">Linkedin</span>
-                    <div class="row__subtitle">
-                        <a class="row__link" href="@anuragyadav">@anuragyadav</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!--  -->
-        <hr />
-        <div class="rows rows--languages">
-            <span class="rows__title"> Languages </span>
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/hindi.svg" alt="" />
-                </div>
-                <div class="row__wrapper">
-                    <span class="row__title">Hindi</span>
-                    <div class="row__subtitle">
-                        <span class="row__text">Native</span>
-                    </div>
-                </div>
-            </div>
-            <!--  -->
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/english.svg" alt="" />
-                </div>
-                <div class="row__wrapper">
-                    <span class="row__title">English</span>
-                    <div class="row__subtitle">
-                        <span class="row__text">Professional working</span>
-                    </div>
-                </div>
-            </div>
-            <!--  -->
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/spanish.svg" alt="" />
-                </div>
-                <div class="row__wrapper">
-                    <span class="row__title">Spanish</span>
-                    <div class="row__subtitle">
-                        <span class="row__text">Elementary</span>
-                    </div>
-                </div>
-            </div>
-            <!--  -->
-        </div>
-        <!--  -->
-        <hr />
-        <!--  -->
-        <div class="rows rows--hobbies-interests">
-            <span class="rows__title">Hobbies & Interests</span>
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/gaming.svg" alt="" />
-                </div>
-                <span class="row__subtitle">Gaming</span>
-            </div>
-            <!--  -->
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/travelling.svg" alt="" />
-                </div>
-                <span class="row__subtitle">Travelling</span>
-            </div>
-            <!--  -->
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/sketching.svg" alt="" />
-                </div>
-                <span class="row__subtitle">Sketching/Graffitti</span>
-            </div>
-            <!--  -->
-            <div class="row">
-                <div class="row__icon">
-                    <img src="/icons/blogging.svg" alt="" />
-                </div>
-                <span class="row__subtitle">Blogging</span>
-            </div>
-            <!--  -->
-        </div>
+        flair to explore Computer Science Engineering"
+    />
+    <hr />
+    <div class="rows rows--infos">
+      <Row
+        v-for="(item, index) in itemsRows.infos.items"
+        :icon="item.icon"
+        :title="item.title"
+        :link="item.link"
+        :key="index"
+      />
     </div>
+    <hr />
+    <div class="rows rows--socials">
+      <span class="rows__title">Socials</span>
+      <Row
+        v-for="(item, index) in itemsRows.socials.items"
+        :icon="item.icon"
+        :title="item.title"
+        :link="item.link"
+        :key="index"
+      />
+    </div>
+    <hr />
+    <div class="rows rows--languages">
+      <span class="rows__title"> Languages </span>
+
+      <Row
+        v-for="(item, index) in itemsRows.languages.items"
+        :icon="item.icon"
+        :title="item.title"
+        :link="item.link"
+        :key="index"
+      />
+    </div>
+    <hr />
+    <div class="rows rows--hobbies-interests">
+      <span class="rows__title">Hobbies & Interests</span>
+      <Row
+        v-for="(item, index) in itemsRows.hobbies_interests.items"
+        :icon="item.icon"
+        :title="item.title"
+        :key="index"
+      />
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
